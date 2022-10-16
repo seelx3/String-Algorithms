029b0509e607ae5d91d37733e8e1ad213554058e
seelx3 <yuuhashi.26@gmail.com>
2022-10-16 17:05:58 +0900

    add: suffix_tree doubling

--- 01_suffix_tree/comment/my_suffix_tree_doubling.cpp
+++ 01_suffix_tree/comment/my_suffix_tree_doubling.cpp
@@ -13,6 +13,10 @@ vector<int> suffix_array(const string &s) {
   vector<int> tmp(n + 1);
   int k = -1;
 
+#ifdef LOCAL
+  cout << "s = " << s << "\n\n";
+#endif
+
   auto compare_sa = [&n, &rank, &k](const int &i, const int &j) -> bool {
     if (rank[i] != rank[j]) {
       return rank[i] < rank[j];
@@ -31,8 +35,20 @@ vector<int> suffix_array(const string &s) {
   for (k = 1; k <= n; k *= 2) {
     sort(sa.begin(), sa.end(), compare_sa);
 
+#ifdef LOCAL
+    printf("k = %2d\n", k);
+    printf("sa | s[sa[i], %d] | rank(sa[i]) | rank(sa[i]+%d)\n", k, k);
+    for (int i = 0; i <= n; i++) {
+      string tmp = (sa[i] == n ? "-" : s.substr(sa[i], min(k, n - sa[i])));
+      printf("%2d | %-11s | %11d | %11d\n", sa[i], tmp.c_str(), rank[sa[i]],
+             (sa[i] + k <= n ? rank[sa[i] + k] : -1));
+    }
+    cout << "\n";
+#endif
+
     tmp[sa[0]] = 0;
     for (int i = 1; i <= n; i++) {
+      // 右辺2項目は、蟻本の実装と異なる
       tmp[sa[i]] = tmp[sa[i - 1]] + !(rank[sa[i - 1]] == rank[sa[i]] &&
                                       rank[sa[i - 1] + k] == rank[sa[i] + k]);
     }
@@ -50,6 +66,13 @@ int main() {
   cin >> s;
 
   vector<int> sa = suffix_array(s);
+
+#ifdef LOCAL
+  for (int i = 0; i < SZ(sa); i++) {
+    cout << sa[i] << " \n"[i == SZ(sa) - 1];
+  }
+  return 0;
+#endif
 
   int q;
   cin >> q;
